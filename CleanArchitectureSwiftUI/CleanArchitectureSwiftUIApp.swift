e911fa6ce260904edb53a5ee414ff4a9464f6993
Dima Shvets <dmytro.shvets@windmill.ch>
2021-02-15 18:24:46 +0200

    Initial Commit

---
+++ CleanArchitectureSwiftUI/CleanArchitectureSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanArchitectureSwiftUIApp.swift
+//  CleanArchitectureSwiftUI
+//
+//  Created by Dima Shvets on 15.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct CleanArchitectureSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
